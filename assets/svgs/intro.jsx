4a6fc8dba6020ec6ffe28f2930f3ee7a619cfb8b
phc-quangg <quanggtangg@gmail.com>
2023-05-04 17:45:38 +0700

    Updates

---
+++ assets/svgs/intro.jsx
@@ -0,0 +1,6 @@
+export const Aasdfsf = () => (
+    <svg class="w-10 h-10" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M8 13.175C14.9 6.275 26.2 6.275 33.1 13.175C40 20.075 40 31.375 33.1 38.275" stroke="currentColor" strokeWidth="15" strokeMiterlimit="10" stroke-linecap="round" strokeLinejoin="round"></path>
+        <path d="M8 13.175C14.9 6.275 26.2 6.275 33.1 13.175C40 20.075 40 31.375 33.1 38.275" stroke="currentColor" strokeOpacity="0.2" stroke-width="15" strokeMiterlimit="10" stroke-linecap="round" strokeLinejoin="round"></path>
+    </svg>
+);
